0e9e09fa27a1ef014465542c65b7335deb3f2d89
dishaShah01 <dishashah.shah40@gmail.com>
2021-09-17 22:57:53 +0530

    frontend

--- app.py
+++ app.py
@@ -8,23 +8,31 @@ app = Flask(__name__)
 @app.route('/')
 def uploady_file():
     print("Hello")
-    return render_template('upload.html')
+    return render_template('index.html')
 
 
 @app.route('/uploader', methods=['GET', 'POST'])
 def upload_file():
     if request.method == 'POST':
-        f = pd.read_csv(request.files['csvfile'])
-        print(f)
+        #f = pd.read_csv(request.files['csvfile'])
+        #print(f)
         # data = []
         # with open(f) as file:
         #     csvfile = csv.reader(file)
         #     for row in csvfile:
         #         data.append(row)
         # print(data)
-        # return render_template('data.html', data=data)
-        return 'file uploaded successfully'
+        #get number of layers, preferably produce a list
 
+        layers=[i for i in range(1,10)]
+        return render_template('layers.html', layers=layers)
+        #return 'file uploaded successfully'
+@app.route('/layers', methods=['GET', 'POST'])
+def getlayers():
+    if request.method == 'POST':
+        
+        return render_template('', layers=layers) #3 to test
+    
 
 if __name__ == '__main__':
     app.run(debug=True)
